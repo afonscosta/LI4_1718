a4889a5f9c7189dd6987de8eb219806220b724df
Diogo Afonso Silva Costa <afonso.costa.15@gmail.com>
2018-05-15 16:10:13 +0100

    Adicionado o projeto BreadSpread

---
+++ Fase3/BreadSpread/BreadSpread/Funcionario.cs
@@ -0,0 +1,64 @@
+namespace BreadSpread
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Funcionario")]
+    public partial class Funcionario
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Funcionario()
+        {
+            Encomendas = new HashSet<Encomenda>();
+        }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int idFunc { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string nome { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateTime dataNasc { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string contacto { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string rua { get; set; }
+
+        public int numPorta { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string codPostal { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string cidade { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string password { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string freguesia { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string estadoConta { get; set; }
+
+        public bool distribuicao { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Encomenda> Encomendas { get; set; }
+    }
+}
