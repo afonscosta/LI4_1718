ec4bbf8996339686a6e53f916d214a8c5cf77c88
Afonso Costa <afonso.costa.15@gmail.com>
2018-05-31 11:00:45 +0100

    Adicionadas views do Admin, Estafeta, Padeiro.

--- Fase3/BreadSpread/BreadSpread/Models/Funcionario.cs
+++ Fase3/BreadSpread/BreadSpread/Models/Funcionario.cs
@@ -16,7 +16,7 @@ namespace BreadSpread.Models
         }
 
         [Key]
-        public int idFunc { get; set; }
+        public string idFunc { get; set; }
 
         [Required]
         [StringLength(100)]
