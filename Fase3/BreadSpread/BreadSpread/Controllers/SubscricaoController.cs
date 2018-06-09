0998dc71a54ea8760f1f4837fbfa7f3426ed13ce
Marco Silva <marant_silva@hotmail.com>
2018-06-09 16:27:51 +0100

    adicionadas funções para adição das encomendas à base de dados

--- Fase3/BreadSpread/BreadSpread/Controllers/SubscricaoController.cs
+++ Fase3/BreadSpread/BreadSpread/Controllers/SubscricaoController.cs
@@ -34,10 +34,43 @@ namespace BreadSpread.Controllers
     
         public ActionResult AdicionarEncomendas()
         {
+
+            return View();
+        }
+
+        [HttpPost]
+        public void adicionaEnc([Bind(Include =
+                "idCli, dataEnt, custo, estado, idFunc, rua, numPorta, cosPostal, cidade, obs, freguesia, tipoEnc, dataPag, modoPag, fatura")]
+            Encomenda encomenda)
+        {
+
             //nome de utilizador que tem login feito
             var User_In_Session = User.Identity.Name;
 
-            return View();
+            var encomendas = (from m in db.Encomendas
+                              where m.idCli.ToString() == User_In_Session
+                              select m);
+
+            int lastEncID = encomendas.Max(item => item.idEnc);
+
+            //inserir associação entre a encomenda e os produtos
+            //adicionaProdEnc(produtos);
+        }
+
+        public void adicionaProdEnc(List<Tuple<int, int>> produtos, int lastEncID)
+        {
+            int tamanho_list = produtos.Count;
+            for (int i = 0; i < tamanho_list; i++)
+            {
+                var enc_prod = new Encomenda_Produto();
+                enc_prod.idEnc = lastEncID;
+                enc_prod.idProd = produtos[i].Item1;
+                enc_prod.quant = produtos[i].Item2;
+
+                db.Encomenda_Produto.Add(enc_prod);
+                db.SaveChanges();
+
+            }
         }
     }
 }
